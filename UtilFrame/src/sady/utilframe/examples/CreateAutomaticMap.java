416f5fcd08ba2c06813a1ad58ebeebb2a27f4286
sady <sadybr@gmail.com>
2014-07-31 10:34:26 -0300

    Automatic map generator

---
+++ UtilFrame/src/sady/utilframe/examples/CreateAutomaticMap.java
@@ -0,0 +1,14 @@
+package sady.utilframe.examples;
+
+import util.DBObjectGenerator;
+
+public class CreateAutomaticMap {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		DBObjectGenerator.main(args);
+	}
+
+}
